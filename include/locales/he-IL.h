39439cf4fee799b3030c519c064c37df22a3d0db
mihael5 <michael.eliad@gmail.com>
2021-05-21 11:14:16 +0300

    Create he-IL.h

---
+++ include/locales/he-IL.h
@@ -0,0 +1,34 @@
+#ifndef LANG_HE_H
+#define LANG_HE_H
+
+// General terms
+#define LANG_CONNECT "התחבר"
+#define LANG_CONNECTED "מחובר"
+#define LANG_DISCONNECT "התנתק"
+
+// App: Time from web
+#define LANG_TFW_UPDATE "עדכן"
+#define LANG_MANUALLY "באופן ידני"
+#define LANG_CANCEL "בטל"
+#define LANG_SAVE "שמור"
+
+// App: BLE media control
+#define LANG_BMC_VOLUME "עוצמה"
+#define LANG_BMC_CONNECTING "מתחבר..."
+
+// App: Stopwatch
+#define LANG_STW_START "התחל"
+#define LANG_STW_STOP "עצור"
+#define LANG_STW_RESET "איפוס"
+#define LANG_STW_CONTINUE "המשך"
+
+// Weekdays
+#define LANG_MONDAY "שני"
+#define LANG_TUESDAY "שלישי"
+#define LANG_WEDNESDAY "רביעי"
+#define LANG_THURSDAY "חמישי"
+#define LANG_FRIDAY "שישי"
+#define LANG_SATURDAY "שבת"
+#define LANG_SUNDAY "ראשון"
+
+#endif
